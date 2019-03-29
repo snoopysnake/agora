23b2397de983d604fe20f2f1ea196eaa693ed000
snoopysnake <chiang4@gmail.com>
2019-03-29 19:49:43 -0400

    fixed button bugs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+var timeout;
+
 $('.button').on('click',function() {
 	console.log('Added to cart.');
 	$('.button-text').toggleClass('button-text-animate');
 	$('.button-text').toggleClass('button-text-after');
 	$('.button-text').toggleClass('button-text-current');
 	$('.button-text').html('ADDED!');
-	window.setTimeout(function() {
+	if (timeout != null) {
+		window.clearTimeout(timeout);
+	}
+	timeout = window.setTimeout(function() {
 		if ($('.button-text-current').text() == 'ADDED!') {
 			console.log('change');
 			$('.button-text-current').html('ADD TO CART');
